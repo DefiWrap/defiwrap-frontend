8dcef478b9aaf6415b06d50d3eaefcc9e545dd89
Mayur Bagga <mayurbagga@hotmail.com>
2023-05-24 15:45:58 +0530

    responsive bug fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,10 @@ export default function Landing() {
       <>
         <div className={styles.container}>
             <div className={ styles.main }>
-                <Card  h="700px" w="100%" backgroundImage={"./bg.jpg"} backgroundSize={"contain"} backgroundBlendMode={"darken"}  variant="outline" borderRadius={ "3xl" }> 
-                        <Flex style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center", marginTop:"100px",margin:"1.5rem"}} >
+                    <Card
+                        style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center",}}
+                        h="70%" w="100%" backgroundImage={ "./bg.jpg" } backgroundSize={ "contain" } backgroundBlendMode={ "darken" } variant="outline" borderRadius={ "3xl" }> 
+                        <Flex style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center", marginTop:"120px",margin:"1.5rem"}} >
                             <Heading textAlign={"center"} color={"pink.500"} >Better Investment Metrics</Heading>
                              <Heading textAlign={"center"} size={"xl"}> Streamline Your Investments with a Diverse DEX Portfolio</Heading>
                              <Heading textAlign={"center"} mt={4} size={"md"}> Simplify Your Investment Strategy with Decentralized Dollar-Cost Averaging </Heading>
@@ -26,17 +28,17 @@ export default function Landing() {
                             <Button  variant={"outline"} colorScheme="pink" leftIcon={<FaVideo></FaVideo>}>Watch Video</Button>
                             
                         </Flex>
-                        <Flex className="drop-in-2" style={{display:"flex",flexDirection:"row", justifyContent:"center", animation:"ease-in"}} >
+                        <Flex style={{display:"flex",flexDirection:"row", justifyContent:"center", animation:"ease-in"}} >
                             <img  style={ { marginTop:"50px", borderStyle:"solid", borderRadius:"1rem",borderWidth:"5px", borderColor:"#EA4C89"} } width={ "80%" } src='./swap_new_final.png'></img>
                         </Flex>
                 </Card>
-                <Box className={styles.topmobilemargin} mt={'20rem'} mb={"70px"} w="100%" borderRadius={ "3xl" }> 
+                <Box className={styles.topmobilemargin}  mb={"70px"} w="100%" borderRadius={ "3xl" }> 
                         <Flex style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center", marginTop:"100px"}} >
                             <Heading  color={"pink.500"}>Hight Impact Results</Heading>
                              <Heading textAlign={"center"} mt={3} size={"xl"}>Strategies Focused on Your Performance</Heading>
                              <Heading textAlign={"center"} mt={4} size={"md"}>We help our clients achieve tangible, high-impact results.</Heading>
                         </Flex>
-                        <SimpleGrid display={"flex"} flexDirection={"row"} flexWrap={"wrap"} spacing={4} mt={"30px"} >
+                        <SimpleGrid display={"flex"} flexDirection={"row"} justifyContent={"center"} flexWrap={"wrap"} spacing={4} mt={"30px"} >
                             <Card borderRadius={"xl"} >
                                 <CardBody style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center"}} >
                                    <Heading color="pink.500" size='xl'><FaMoneyCheck></FaMoneyCheck></Heading>
@@ -58,13 +60,13 @@ export default function Landing() {
                                     <Text textAlign={"center"} mt={2}>View a summary of all your customers over the last month.</Text>
                                 </CardBody>
                             </Card>
-                            <Card borderRadius={"xl"} >
+                            {/* <Card borderRadius={"xl"} >
                                 <CardBody style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center"}} >
                                    <Heading color="pink.500" size='xl'><FaDochub></FaDochub></Heading>
                                     <Heading mt={5} size='md'>No KYC Required</Heading>
                                     <Text textAlign={"center"} mt={2}>View a summary of all your customers over the last month.</Text>
                                 </CardBody>
-                            </Card>
+                            </Card> */}
                             
                             
                         </SimpleGrid>
